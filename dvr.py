a5284a005c3a2f6825cb43d84d3abaac5c9fc46f
Jonathan Liu <95923788+jl6078@users.noreply.github.com>
2025-04-28 18:44:15 -0400

    Add listening for messages

--- dvr.py
+++ dvr.py
@@ -4,6 +4,9 @@ import time
 
 import threading
 
+from LinkedList import Blockchain, Block
+from network import Node
+
 class NetworkInterface():
     """
     DO NOT EDIT.
@@ -66,12 +69,24 @@ class NetworkInterface():
         """
         return self.sock.recv(length)
     
-    def listen_for_messages(self):
+    def listen_for_messages(self, blockchain, nodeWrapper):
+        depth = 0
         while True:
             try:
                 data = self.recv(4096)
                 if data:
                     print(f"\n[Received] {data.decode()}\n> ", end="", flush=True)
+
+                message_dict = data.decode().strip()
+                if message_dict["type"] == "new_neighbor":
+                    # Add new neighbor to the node's neighbors list
+                    nodeWrapper.neighbors.append(message_dict["neighbor_id"])
+                    print(f"New neighbor added: {message_dict['neighbor_id']}")
+                elif message_dict["type"] == "new_block":
+                    block = Block(depth, blockchain.get_latest_block().hash, time.time(), self.message_dict["transactions"], nodeWrapper.neighbors)
+                    blockchain.add_block(block)
+                    print(f"New block added: {block}")
+                
             except Exception as e:
                 print(f"Error receiving data: {e}")
                 break
@@ -135,8 +150,6 @@ def send_user_blocks():
             print(f"Error sending data: {e}")
             break
 
-    
-
 
 if __name__ == '__main__':
     network_ip = sys.argv[1] # the IP address of the network
@@ -158,68 +171,17 @@ if __name__ == '__main__':
     neighbors = init_message.split(". ")[1].split(",") 
 
 
+    blockchain = Blockchain()
+
+    nodeWrapper = Node(node_id)
+
+    nodeWrapper.neighbors = neighbors
+
     # Start the listener thread
-    listener_thread = threading.Thread(target=net_interface.listen_for_messages, daemon=True)
+    listener_thread = threading.Thread(target=net_interface.listen_for_messages, args=(blockchain, nodeWrapper,), daemon=True)
     listener_thread.start()
-
-
 
 
     # Main thread handles user input
     send_user_blocks()
-
-
-
-
-
-
-
-
-
-
-    # """Initialize dv table with the costs of direct neighbors"""
-    # for pair in neighbors_pairs:
-    #     neighbor_id, cost = pair.split(":") 
-    #     if neighbor_id not in neighbors:
-    #         neighbors.append(neighbor_id)
-    #         direct_link_costs[neighbor_id] = int(cost) 
-
-    #     dv_table.setdefault(neighbor_id, []).append((neighbor_id, int(cost)))
-
-    # """Log initial distance vector"""
-    # log_file = open(f"log_{node_id}.txt", "w") 
-    # write_log(node_id, dv_table)
-    
-    """Extract DV from DV table"""
-    distance_vector = get_distance_vector(dv_table) 
-
-    """Send the initial distance vector to neighbors"""
-    message = dv_to_message(node_id, distance_vector)
-    net_interface.send(message)
-
-    while True:
-        message = net_interface.recv(4096) 
-
-        messages = message.decode().strip().split("\n")  
-
-        all_node_dvs = {}  
-
-        for msg in messages:
-            if msg.strip():  
-                recv_node_id, recv_dv = parse_message(msg)
-                all_node_dvs[recv_node_id] = recv_dv
-
-        for recv_node_id, recv_dv in all_node_dvs.items():
-            new_dv_table, new_dv = update_dv(node_id, recv_node_id, dv_table, recv_dv, direct_link_costs)
-
-            dv_table = new_dv_table
-            print("DV table updated")
-
-            if new_dv != distance_vector:
-                print(f"Updated distance vector: {new_dv}")
-                distance_vector = new_dv
-                message = dv_to_message(node_id, new_dv)
-                net_interface.send(message)
-                write_log(node_id, dv_table)  
-
 
